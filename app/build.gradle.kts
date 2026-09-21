b9ff3c132bfad33d2c34d856b67d0c46394cde00
3115a083 <9198182+3115a083@users.noreply.github.com>
2026-09-22 00:47:54 +0200

    Align Java and Kotlin JVM targets

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,6 +14,10 @@ android {
         versionName = "0.1.0"
     }
     buildFeatures { compose = true }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
     testOptions { unitTests.isReturnDefaultValues = true }
 }
 dependencies {
